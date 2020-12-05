b88facd49a99b56b263cd157e082ec44d9cb0a2b
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-12-04 19:43:40 -0500

    beginning groundwork

--- Client/src/main/java/controllers/TransactionController.java
+++ Client/src/main/java/controllers/TransactionController.java
@@ -1,7 +1,28 @@
 package controllers;
 
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class TransactionController {
     private String rootURL = "http://zipcode.rocks:8085";
+    private OkHttpClient okHttpClient = new OkHttpClient();
+    public TransactionController() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .connectTimeout(5, TimeUnit.MINUTES)
+                .readTimeout(5, TimeUnit.MINUTES)
+                .writeTimeout(5, TimeUnit.MINUTES)
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        //RequestBody body = RequestBody.create(mediaType, data);
+        Request request = new Request.Builder()
+                .url(rootURL)
+                .method("GET", null)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+    }
 
-    public TransactionController() {}
 }
+
