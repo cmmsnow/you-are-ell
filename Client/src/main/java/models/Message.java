ce9b72af8608189583108f2cbefb7f2c9bc5126b
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-12-05 16:35:04 -0500

    message and message text view

--- Client/src/main/java/models/Message.java
+++ Client/src/main/java/models/Message.java
@@ -1,17 +1,69 @@
 package models;
 
-/* 
+import views.IdTextView;
+import views.MessageTextView;
+
+/*
  * POJO for an Message object
  */
 public class Message {
     String message;
-    String fromId;
-    String toId;
+    String fromid;
+    String toid;
+    String sequence;
+    String timestamp;
 
-    public Message (String message, String fromId, String toId) {
+    public Message (String message, String fromid, String toid, String sequence, String timestamp) {
         this.message = message;
-        this.fromId = fromId;
-        this.toId = toId;
+        this.fromid = fromid;
+        this.toid = toid;
+        this.sequence = sequence;
+        this.timestamp = timestamp;
     }
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getFromid() {
+        return fromid;
+    }
+
+    public void setFromid(String fromid) {
+        this.fromid = fromid;
+    }
+
+    public String getToid() {
+        return toid;
+    }
+
+    public void setToid(String toid) {
+        this.toid = toid;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        MessageTextView messageTextView = new MessageTextView(this);
+        return messageTextView.toString();
+    }
 }
--- Client/src/main/java/views/MessageTextView.java
+++ Client/src/main/java/views/MessageTextView.java
@@ -3,11 +3,13 @@ package views;
 import models.Message;
 
 public class MessageTextView {
+    Message message;
 
     public MessageTextView(Message msgToDisplay) {
+        this.message = msgToDisplay;
     }
 
     @Override public String toString() {
-        return null;
+        return String.format("\nSequence: %s,  Timestamp: %s,  From: %s,  To: %s,  Message: %s", message.getSequence(), message.getTimestamp(), message.getFromid(), message.getToid(), message.getMessage());
     } 
 }
