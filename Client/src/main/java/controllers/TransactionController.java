a4e9cdbe96c625516040f9a2662482808b3c8c8e
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-12-06 09:02:28 -0500

    adjusting youareell and transaction controller to prep for messages

--- Client/src/main/java/controllers/TransactionController.java
+++ Client/src/main/java/controllers/TransactionController.java
@@ -9,6 +9,7 @@ public class TransactionController {
     private String rootURL = "http://zipcode.rocks:8085";
     private OkHttpClient client;
     private MediaType mediaType;
+    //private RequestBody json;
 
     public TransactionController() throws IOException {
         client = new OkHttpClient();//.newBuilder()
@@ -16,9 +17,8 @@ public class TransactionController {
 //                .readTimeout(5, TimeUnit.MINUTES)
 //                .writeTimeout(5, TimeUnit.MINUTES)
 //                .build();
-        mediaType = MediaType.parse("application/json; charset=utf-8");
-        //mediaType = MediaType.get("application/json; charset=utf-8");
-        //RequestBody body = RequestBody.create(mediaType, data);
+        mediaType = MediaType.parse("application/json; charset=utf-8"); //Lake used ".get()"
+        //json = RequestBody.create(mediaType, data);
     }
 
     public String get(String path) throws IOException {
@@ -31,23 +31,23 @@ public class TransactionController {
         return response.body().string();
     }
 
-    public String post(String path) throws IOException {
+    public String postIds(String path) throws IOException {
         String body="{\n" +
                 "        \"userid\": \"-\",\n" +
-                "        \"name\": \"Waterlicious\",\n" +
-                "        \"github\": \"Yoyo\"\n" +
+                "        \"name\": \"KillMoi\",\n" +
+                "        \"github\": \"ZZtop\"\n" +
                 "    }";
         RequestBody json = RequestBody.create(mediaType, body);
         Request request = new Request.Builder()
                 .url(rootURL + path)
                 .method("POST", json)
-                .addHeader("Content-Type", "application/json")
+                //.addHeader("Content-Type", "application/json")
                 .build();
         Response response = client.newCall(request).execute();
         return response.body().string();
     }
 
-    public String put(String path) throws IOException {
+    public String putIds(String path) throws IOException {
         String body="{\n" +
                 "        \"userid\": \"b46ba467a94e24292ae392310315b72316c8523d\",\n" +
                 "        \"name\": \"WhippedByThisCourse\",\n" +
@@ -57,7 +57,7 @@ public class TransactionController {
         Request request = new Request.Builder()
                 .url(rootURL + path)
                 .method("PUT", json)
-                .addHeader("Content-Type", "application/json")
+                //.addHeader("Content-Type", "application/json")
                 .build();
         Response response = client.newCall(request).execute();
         return response.body().string();
--- Client/src/main/java/youareell/YouAreEll.java
+++ Client/src/main/java/youareell/YouAreEll.java
@@ -15,7 +15,7 @@ public class YouAreEll {
     private IdController idCtrl;
     private TransactionController transactionController;
 
-    public YouAreEll (MessageController m, IdController j, TransactionController t) {
+    public YouAreEll(MessageController m, IdController j, TransactionController t) {
         this.msgCtrl = m;
         this.idCtrl = j;
         this.transactionController = t;
@@ -27,23 +27,25 @@ public class YouAreEll {
         //System.out.println(urlhandler.methodSorter("/ids", "POST", ""));
         //System.out.println(urlhandler.methodSorter("/ids", "PUT", ""));
         //System.out.println(urlhandler.methodSorter("/messages", "GET", ""));
+        //System.out.println(urlhandler.methodSorter("/messages", "POST", ""));
+        //System.out.println(urlhandler.methodSorter("/messages", "PUT", ""));
         //Change ids put & post when i get user input from SimpleShell?
         //Add more of these for messages
     }
 
     public boolean methodSorter(String mainurl, String method, String jpayload) throws IOException {
         if (method.equals("GET")) getURLCall(mainurl, jpayload);
-        if (method.equals("POST")) postURLCall(mainurl, jpayload);
-        if (method.equals("PUT")) putURLCall(mainurl, jpayload);
+        if (method.equals("POST") && mainurl.equals("/ids")) postIdsURLCall(mainurl, jpayload);
+        if (method.equals("PUT") && mainurl.equals("/ids")) putIdsURLCall(mainurl, jpayload);
         return true;
     }
 
     public String getURLCall(String mainurl, String jpayload) throws IOException {
-        if (mainurl.equals("/ids")){
+        if (mainurl.equals("/ids")) {
             String response = transactionController.get(mainurl);
             ArrayList<Id> idsList = idCtrl.getIds(response);
             System.out.println(idsList);
-        } else if (mainurl.equals("/messages")){
+        } else if (mainurl.equals("/messages")) {
             String response = transactionController.get(mainurl);
             ArrayList<Message> messagesList = msgCtrl.getMessages(response);
             System.out.println(messagesList);
@@ -51,26 +53,22 @@ public class YouAreEll {
         return "nada";
     }
 
-    public String postURLCall(String mainurl, String jpayload) throws IOException {
-        if (mainurl.equals("/ids")){
-            String response = transactionController.post(mainurl);
+    public String postIdsURLCall(String mainurl, String jpayload) throws IOException {
+        if (mainurl.equals("/ids")) {
+            String response = transactionController.postIds(mainurl);
             ArrayList<Id> idsList = idCtrl.getIds(response);
             System.out.println(idsList);
-        } else if (mainurl.equals("/messages")){
+            return "nada";
+        }
+
+        public String putIdsURLCall(String mainurl, String jpayload) throws IOException {
+            if (mainurl.equals("/ids")) {
+                String response = transactionController.putIds(mainurl);
+                ArrayList<Id> idsList = idCtrl.getIds(response);
+                System.out.println(idsList);
+                return "nada";
+            }
 
         }
-        return "nada";
     }
-
-    public String putURLCall(String mainurl, String jpayload) throws IOException {
-        if (mainurl.equals("/ids")){
-            String response = transactionController.put(mainurl);
-            ArrayList<Id> idsList = idCtrl.getIds(response);
-            System.out.println(idsList);
-        } else if (mainurl.equals("/messages")){
-
-        }
-        return "nada";
-    }
-
 }
